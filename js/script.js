699280e846bb2d1eb95e245bbc9fea70bcc75508
Juan Almeida <117415730+jnalst@users.noreply.github.com>
2023-07-31 17:48:46 -0300

    Menu Hambuger realizado🍔
    Responsividade✔

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
 /* SWIPPER JS  -- GALERIA -- */
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Default parameters
   pagination: {
     el: ".swiper-pagination",
-  },  
-   slidesPerView: 1,
+  },
+  slidesPerView: 1,
   spaceBetween: 30,
   // Responsive breakpoints
   breakpoints: {
-   // when window width is >= 480px
+    // when window width is >= 480px
     480: {
       slidesPerView: 1,
-      spaceBetween: 30
+      spaceBetween: 30,
     },
     // when window width is >= 640px
     640: {
       slidesPerView: 1,
-      spaceBetween: 30
-    }, 1100: {
+      spaceBetween: 30,
+    },
+    1100: {
       slidesPerView: 2,
-      spaceBetween: 20}
+      spaceBetween: 20,
+    },
+  },
+});
 
+// -----------------------------------------------
+
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", "true");
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
   }
-})
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
